3cc9b3b1559519eba33a04742590e90566d76b3a
karenvargasr <karen.vargas.r@gmail.com>
2019-07-02 12:04:23 -0400

    first commit

--- src/data.js
+++ src/data.js
@@ -2,9 +2,8 @@
 
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
+ const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.data = RICKANDMORTY.results;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-/* Manejo del DOM */
+/* Manejo del DOM 
+let dataName= "";
+const data= RICKANDMORTY.results;
+for (let i= 0; i< 4; i++){
+  dataName += (data[i].name); 
+}
+const containerCards =document.getElementById("cards");
+document.getElementById('onload').addEventListener('click', () => {
+    containerCards.innerHTML = `<h2> 
+   cafeeeeeeeeeeeeeeeeee ${name}
+  </h2>`
+})*/
+
+//
