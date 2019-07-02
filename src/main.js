d301a1529dc3288289e79e4dd43a2d798996f574
NatRV <n.riquelme.veliz@gmail.com>
2019-07-02 09:50:09 -0400

    Agregando CSS, mainjs actualizado, imagenes de formulario

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-/* Manejo del DOM */
+window.onload = function () {
+    // accedemos a la lista de pokemones
+    const pokemons = POKEMON.pokemon;
+    const pokeSection = document.getElementById('poke-section');
+  
+    for (i = 0; i < pokemons.length;i++)
+    {
+      let divNum = "div" + pokemons[i].num;
+      let newDiv = document.createElement("div");
+      let textNode = document.createTextNode(pokemons[i].name);
+      newDiv.appendChild(textNode);
+      newDiv.setAttribute("id", divNum);
+      let pokeIMG = document.createElement("img");
+      pokeIMG.setAttribute("src", pokemons[i].img);
+      newDiv.appendChild(pokeIMG);
+      pokeSection.appendChild(newDiv);
+    }
+  };
