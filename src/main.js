bc3165bc3740239513e7e6d2e6d3dc70912aa4a9
Ayma27 <aymacobain@gmail.com>
2019-07-04 08:36:03 -0400

    data lovers con data mostrandose

--- src/main.js
+++ src/main.js
@@ -1,16 +1,38 @@
 /* Manejo del DOM */
+//Se crea un select
+document.getElementById("pageCharacters").innerHTML =`<select class="" id="select">
+<option value=""></option>
+<option value="name">nombre</option>
+<option value="status">estado</option>
+<option value="run">rut</option>
+</select>;`
+//vairable para filtrar en select
+const selectFilter = document.getElementById("select");
+selectFilter.addEventListener("change", () => {
+let condition = selectFilter.options[selectFilter.selectedIndex].value;
+console.log(condition);
+});
+//leer data
 const dataRickAndMorty = RICKANDMORTY.results;
 
 for (let i = 0; i <dataRickAndMorty.length; i++) {
-  var rickYMorty= dataRickAndMorty.length;
-  console.log(rickYMorty);
-
+  console.log(dataRickAndMorty[i].name);
+  console.log(dataRickAndMorty[i].status);
+  console.log(dataRickAndMorty[i].image);
+  let card = document.createElement("div");
+  let cardName = document.createElement("h2");
+  let cardStatus = document.createElement("h3");
+  let cardImg = document.createElement("img");
+//Le dice de donde optiene el valor
+  card.className = "cards";
+  cardImg.src = dataRickAndMorty[i].image;
+  cardName.textContent = dataRickAndMorty[i].name;
+  cardStatus.textContent = dataRickAndMorty[i].status;
+// Valores que mostrara el div
+  card.appendChild(cardImg);
+  card.appendChild(cardName);
+  card.appendChild(cardStatus);
+//imprimir
+document.getElementById("pageCharacters").appendChild(card).innerHTML;
 };
-
-
-
-
-document.getElementById('buttonWorld').addEventListener("click", () => {
-
-}
-)
+//document.getElementById('buttonCharacters').addEventListener("click", () => {});//
