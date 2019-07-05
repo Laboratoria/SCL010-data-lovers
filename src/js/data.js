a3294331e520e44cb96fd12761ca444f72f79d5b
Andrea Gonzalez <gogoz.gonzalez@gmail.com>
2019-07-05 10:17:22 -0400

    agregando filtros a las tarjetas

--- src/js/data.js
+++ src/js/data.js
@@ -1,2 +1,35 @@
+const loadData = () => {
+  const data = RICKANDMORTY.results;
+  return data;
+};
 
+window.loadData = loadData;
 
+const filterSpecies = (specie) => {
+  const data = RICKANDMORTY.results;
+  let filterData = [];
+  for (let i = 0; i < data.length; i++) {
+
+    if (data[i].species == specie) {
+        filterData.push(data[i]);
+    }
+  }
+  return filterData;
+};
+
+window.filterSpecies = filterSpecies;
+
+const filterGender = (gender) => {
+    const data = RICKANDMORTY.results;
+    let filterData = [];
+    for (let i = 0; i < data.length; i++) {
+  
+      if (data[i].gender == gender) {
+          filterData.push(data[i]);
+      }
+    }
+    return filterData;
+  };
+  
+  window.filterGender = filterGender;
+  
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,7 @@
-const loadData = () => {
-  const data = RICKANDMORTY.results;
-  return data;
-};
-
 function init() {
   let characterData = loadData();
-  for (i = 0; i < characterData.length; i++) {
+  for (i = 0; i < 40; i++) {
+    // for (i = 0; i < characterData.length; i++) {
     document.getElementById('characters').innerHTML += `
       <div class="card">
         <img src="${characterData[i].image}" alt="">
@@ -15,4 +11,31 @@ function init() {
   }
 }
 init();
+
+const filterSpices = document.getElementById('specie');
+  filterSpices.addEventListener('change',() =>{
+    let characterData = filterSpecies(filterSpices.value);
+    document.getElementById("characters").innerHTML="";
+    for (i = 0; i < characterData.length; i++) {
+    document.getElementById('characters').innerHTML += `
+      <div class="card">
+        <img src="${characterData[i].image}" alt="">
+        <h3>${characterData[i].name}</h3>
+        <p>Especie: ${characterData[i].species}</p>
+      </div>`;
+      }
+   })
+
+   const filterGender = document.getElementById('gender');
+   filterSpices.addEventListener('change',() =>{
+     let characterData = filterGender(filterGender.value);
+     document.getElementById("characters").innerHTML="";
+     for (i = 0; i < characterData.length; i++) {
+     document.getElementById('characters').innerHTML += `
+       <div class="card">
+         <img src="${characterData[i].image}" alt="">
+         <h3>${characterData[i].name}</h3>
+         <p>Especie: ${characterData[i].species}</p>
+       </div>`;
+   }})
 
