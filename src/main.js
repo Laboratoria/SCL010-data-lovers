a3636a3e695bfd4e12856a21b55232e94c0c8109
natalie-veloso <natalie.veloso.z@gmail.com>
2019-07-05 09:03:05 -0400

    orden de a-z listo

--- src/main.js
+++ src/main.js
@@ -1,59 +1,106 @@
 //recorrer data
 window.onload=() =>{
+
+  //ordenar
+const pokeOrder = document.getElementById("name");pokeOrder.addEventListener('change', () => {
+  let sortOrder = pokeOrder.value;
+  let selectionOrder = sortPokes(myData,"name",sortOrder);
+  container.innerHTML ="";    selectionOrder.forEach(element=> {
+      container.innerHTML += `
+      <div class="flip-card">
+ <div class="flip-card-inner">
+
+   <div class="flip-card-front">
+     <img id="img-card" src="${element.img}" alt="Avatar">
+    </div>
+
+    <div class="flip-card-back">
+      <p>${element.name}</p> 
+      <p id="type-poke">Tipo:${element.type}</p> 
+      <p"number-poke">${element.num}</p>
+    </div>
+  </div>
+ </div>`  
+ 
+
+  })
+ })
   
 //const para acceder a la data
 const myData = POKEMON.pokemon; 
 
 //DOM FUNCIÓN FILTRAR
-const container = document.getElementById("root"); 
-document.getElementById("pok-filter").addEventListener("change",() => {
+const container = document.getElementById("root");
+
+document.getElementById("pok-filter").addEventListener("change",() => {// esta imprimiendo el resultado solo en el select, juntar las cartas con el filtro//
    let tipoFilter = document.getElementById("pok-filter").value;
    let lastResult = window.filterPoke(myData,tipoFilter);
    container.innerHTML ="";
    lastResult.forEach(element => {
        container.innerHTML += `
-       <div class="img">
-    <img src="${element.img}">
+   
+    <div class="flip-card">
+ <div class="flip-card-inner">
+
+   <div class="flip-card-front">
+     <img id="img-card" src="${element.img}" alt="Avatar">
     </div>
-    <h3>${element.type}</h3>
-    <p>${element.name}</p>`    
+
+    <div class="flip-card-back">
+      <p>${element.name}</p> 
+      <p id="type-poke">Tipo:${element.type}</p> 
+      <p"number-poke">${element.num}</p>
+    </div>
+  </div>
+ </div>`  
+
   })
+
 
    })
    
- 
+//   showcards(POKEMON.pokemon)
 
 
-//document.getElementById('pokemon-list').innerHTML='';
-// for(let i=0; i<POKEMON.pokemon.length; i++){
+//  function showcards(filterPoke){
 
- //document.getElementById('pokemon-list').innerHTML+=`
+  
 
- //document.getElementById('root').innerHTML='';
+//  document.getElementById('root').innerHTML='';
 
-//document.getElementById('pokemon-list').innerHTML='';
-// for(let i=0; i<POKEMON.pokemon.length; i++){
+// document.getElementById('pokemon-list').innerHTML='';
+// for(let i=0; i<filterPoke.length; i++){
 
- //document.getElementById('pokemon-list').innerHTML+=`
+//  document.getElementById('pokemon-list').innerHTML+=`
 
-//<div class="col s6 m3 l3">
+// <div class="col s6 m3 l3">
 
-//<div class="flip-card">
- // <div class="flip-card-inner">
+{/* <div class="flip-card">
+ <div class="flip-card-inner">
 
-   // <div class="flip-card-front">
-     // <img id="img-card" src="${POKEMON.pokemon[i].img}" alt="Avatar">
-    //</div>
+   <div class="flip-card-front">
+     <img id="img-card" src="${filterPoke[i].img}" alt="Avatar">
+    </div>
 
-    //<div class="flip-card-back">
-      //<p>${POKEMON.pokemon[i].name}</p> 
-      //<p id="type-poke">Tipo:${POKEMON.pokemon[i].type}</p> 
-      //<p"number-poke">${POKEMON.pokemon[i].num}</p>
+    <div class="flip-card-back">
+      <p>${filterPoke[i].name}</p> 
+      <p id="type-poke">Tipo:${filterPoke[i].type}</p> 
+      <p"number-poke">${filterPoke[i].num}</p>
     //</div>
   //</div>
-// </div>
-// </div>`
+//  </div> */}
+//   </div>`
 
 // }
- 
+{/* <div class="img">
+<img src="${element.img}">
+</div>
+<h3>${element.name}</h3>
+<p>${element.type}</p>`
+// } */}
+
+
+
+
+
 }
