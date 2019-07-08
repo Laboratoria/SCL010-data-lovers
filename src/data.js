dd50c364130a8c59365bceeb7ae9c9819e920737
victmartinez <victmartinez@outlook.com>
2019-07-08 12:09:57 -0400

    FILTRO

--- src/data.js
+++ src/data.js
@@ -1,6 +1,19 @@
 
-window.filterEpisodes = 
-const filterEpisodes = (data, episodes) => {
-  return 'example';
-};
+ //funcion filtrar
+ const filter = (condition) => {
 
+     let filteredData = [""];
+   //nuevo arr donde guardamos las coincidencias del recorrido
+   for (let i = 0; i < data.length; i++){
+    //  console.log(data)
+      //recorrido por dentro de los episodios de los personajes
+     if(data[i].episode === (condition)){ //si dentro de los characters.episodes hay una coincidencia con la condicion episodes lo guardará 
+       filteredData.push(data[i].episode);
+       console.log(filteredData);
+     }
+     
+    }
+ };
+
+//hacemos funcion global
+window.filter = filter;
