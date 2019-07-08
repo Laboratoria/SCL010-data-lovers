70b0f7958a847240aaacb86e6845b8ce1ccdb206
Ayma27 <aymacobain@gmail.com>
2019-07-08 10:49:47 -0400

    correccion error filtrado

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,7 @@ const filterSpices = document.getElementById('specie');
       </div>`;
       }
    });
-   
+
 //filtrado por genero
    const filterGender = document.getElementById('gender');
    filterGender.addEventListener('change',() =>{
@@ -37,42 +37,6 @@ const filterSpices = document.getElementById('specie');
        <div class="card">
          <img src="${characterData[i].image}" alt="">
          <h3>${characterData[i].name}</h3>    <section class="section_label">
-28
-      <label class="label">Especie</label>
-29
-      <select class="filter_label" id="specie">
-30
-        <option value="">Selecciona</option>
-31
-        <option value="Humanoid">Humanoide</option>
-32
-        <option value="Human">Humano</option>
-33
-        <option value="Alien">Extraterreste</option>
-34
-        <option value="Poopybutthole">Poopybutthole</option>
-35
-        <option value="Robot">Robot</option>
-36
-        <option value="Cronenberg">Cronenberg</option>
-37
-        <option value="Animal">Animales</option>
-38
-        <option value="Mytholog">Mitologico</option>
-39
-        <option value="Vampire">Vampiros</option>
-40
-        <option value="Disease ">Enfermedades</option>
-41
-        <option value="Parasite">Parasitos</option>
-42
-        <option value="unknown">Otros</option>
-43
-      </select>
-44
-​
-45
-
          <p>Status: ${characterData[i].gender}</p>
        </div>`;
    }});
@@ -91,7 +55,3 @@ const filterSpices = document.getElementById('specie');
          <p>Status: ${characterData[i].status}</p>
        </div>`;
    }});
-
-
-
-
