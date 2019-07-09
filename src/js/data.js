92a9826fe3af5715bc018fafa1237ad78bb6a7e5
Andrea Gonzalez <gogoz.gonzalez@gmail.com>
2019-07-09 09:03:38 -0400

    ordenar de ascendente y descendente listo

--- src/js/data.js
+++ src/js/data.js
@@ -5,51 +5,77 @@ const loadData = () => {
 
 window.loadData = loadData;
 
-//filtrado por especie
 
-const filterSpecies = (specie) => {
-  const data = RICKANDMORTY.results;
-  let filterData = [];
-  for (let i = 0; i < data.length; i++) {
+   //seleccion de A-Z y Z-A
+   const selectOrder=document.getElementById('order');
+   selectOrder.addEventListener('change',()=>{
+    let characterData = loadData();
+    let htmlForInyect='';
 
-    if (data[i].species == specie) {
-        filterData.push(data[i]);
+    if(selectOrder.value == '1'){
+      characterData.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+    }else{
+      characterData.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+      characterData.reverse();
     }
-  }
-  return filterData;
-};
- window.filterSpecies = filterSpecies;
- 
- //filtrado por genero
 
- const filterGenders = (gender) => {
-     const data = RICKANDMORTY.results;
-     let filterData = [];
-     for (let i = 0; i < data.length; i++) {
-   
-       if (data[i].gender == gender) {
-          filterData.push(data[i]);
-       }
+    // for (let i = 0; i < 30; i++) {
+       for (let i = 0; i < characterData.length; i++) {
+       htmlForInyect+= `
+         <div class="card">
+           <img src="${characterData[i].image}" alt="">
+           <h3>${characterData[i].name}</h3>
+           <p>Especie: ${characterData[i].species}</p>
+         </div>`;
      }
-     return filterData;
-   };
- 
-   window.filterGenders = filterGenders;
+     document.getElementById('characters').innerHTML = htmlForInyect;
+   });
 
-   //filtrado por estado
-   
-   const filterStatus = (status) => {
-    const data = RICKANDMORTY.results;
-    let filterData = [];
-    for (let i = 0; i < data.length; i++) {
-  
-      if (data[i].status == status) {
-         filterData.push(data[i]);
+
+
+    //filtrado por especie
+
+    const filterSpecies = (specie) => {
+      const data = RICKANDMORTY.results;
+      let filterData = [];
+      for (let i = 0; i < data.length; i++) {
+
+        if (data[i].species == specie) {
+          filterData.push(data[i]);
+        }
       }
-    }
-    return filterData;
-  };
-  
-  window.filterStatus = filterStatus;
+      return filterData;
+    };
+    window.filterSpecies = filterSpecies;
 
-   
+    //filtrado por genero
+
+    const filterGenders = (gender) => {
+      const data = RICKANDMORTY.results;
+      let filterData = [];
+      for (let i = 0; i < data.length; i++) {
+
+        if (data[i].gender == gender) {
+          filterData.push(data[i]);
+        }
+      }
+      return filterData;
+    };
+
+    window.filterGenders = filterGenders;
+
+    //filtrado por estado
+
+    const filterStatus = (status) => {
+      const data = RICKANDMORTY.results;
+      let filterData = [];
+      for (let i = 0; i < data.length; i++) {
+
+        if (data[i].status == status) {
+          filterData.push(data[i]);
+        }
+      }
+      return filterData;
+    };
+
+    window.filterStatus = filterStatus;
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 
 function init() {
   let characterData = loadData();
-  for (i = 0; i < 40; i++) {
+  for (let i = 0; i < 40; i++) {
    // for (i = 0; i < characterData.length; i++) {
     document.getElementById('characters').innerHTML += `
       <div class="card">
@@ -12,12 +12,13 @@ function init() {
   }
 }
 init();
+ 
 //filtrado por especie
 const filterSpices = document.getElementById('specie');
   filterSpices.addEventListener('change',() =>{
     let characterData = filterSpecies(filterSpices.value);
     document.getElementById("characters").innerHTML="";
-    for (i = 0; i < characterData.length; i++) {
+    for (let i = 0; i < characterData.length; i++) {
     document.getElementById('characters').innerHTML += `
       <div class="card">
         <img src="${characterData[i].image}" alt="">
@@ -32,47 +33,11 @@ const filterSpices = document.getElementById('specie');
    filterGender.addEventListener('change',() =>{
      let characterData = filterGenders(filterGender.value);
      document.getElementById("characters").innerHTML="";
-     for (i = 0; i < characterData.length; i++) {
+     for (let i = 0; i < characterData.length; i++) {
      document.getElementById('characters').innerHTML += `
        <div class="card">
          <img src="${characterData[i].image}" alt="">
-         <h3>${characterData[i].name}</h3>    <section class="section_label">
-28
-      <label class="label">Especie</label>
-29
-      <select class="filter_label" id="specie">
-30
-        <option value="">Selecciona</option>
-31
-        <option value="Humanoid">Humanoide</option>
-32
-        <option value="Human">Humano</option>
-33
-        <option value="Alien">Extraterreste</option>
-34
-        <option value="Poopybutthole">Poopybutthole</option>
-35
-        <option value="Robot">Robot</option>
-36
-        <option value="Cronenberg">Cronenberg</option>
-37
-        <option value="Animal">Animales</option>
-38
-        <option value="Mytholog">Mitologico</option>
-39
-        <option value="Vampire">Vampiros</option>
-40
-        <option value="Disease ">Enfermedades</option>
-41
-        <option value="Parasite">Parasitos</option>
-42
-        <option value="unknown">Otros</option>
-43
-      </select>
-44
-​
-45
-
+         <h3>${characterData[i].name}</h3>  
          <p>Status: ${characterData[i].gender}</p>
        </div>`;
    }});
@@ -83,7 +48,7 @@ const filterSpices = document.getElementById('specie');
    selectStatus.addEventListener('change',() =>{
      let characterData = filterStatus(selectStatus.value);
      document.getElementById("characters").innerHTML="";
-     for (i = 0; i < characterData.length; i++) {
+     for (let i = 0; i < characterData.length; i++) {
      document.getElementById('characters').innerHTML += `
        <div class="card">
          <img src="${characterData[i].image}" alt="">
@@ -91,6 +56,22 @@ const filterSpices = document.getElementById('specie');
          <p>Status: ${characterData[i].status}</p>
        </div>`;
    }});
+
+   // filtrado por orden ascendente y descendente
+const filterOrder = document.getElementById('order');
+  filterSpices.addEventListener('change',() =>{
+    let characterData = filterSpecies(filterSpices.value);
+    document.getElementById("characters").innerHTML="";
+    for (let i = 0; i < characterData.length; i++) {
+    document.getElementById('characters').innerHTML += `
+      <div class="card">
+        <img src="${characterData[i].image}" alt="">
+        <h3>${characterData[i].name}</h3>
+        <p>Especie: ${characterData[i].species}</p>
+      </div>`;
+      }
+   });
+     
 
 
 
