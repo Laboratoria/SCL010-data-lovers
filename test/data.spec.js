096d54057d295ef07768fcd801e8af952d1b378e
Andrea Gonzalez <gogoz.gonzalez@gmail.com>
2019-07-10 12:07:35 -0400

    primer test listo

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,24 @@
 global.window = global;
 global.assert = require('chai').assert;
+require('../src/data/rickandmorty/rickandmorty');
 require('../src/data');
 require('./data.spec.js');
 
 //DESCRIBE QUE (A QUIEN) VOY A TESTEAR
-describe('filterSpecies' () =>{
-const data.results = [
-{"name": "Rick Sanchez",
-  "species": "Human"
-},
-{"name": "Morty Smith",
-  "species":"Human"
-}
-]} 
-
-
- it('debería retornar el objeto Rick Sanchez al filtar por especie humano', () =>{
-asser.deepEqual(window.filterSpecies(muestra), "Humano"),
-[{"name":"Rick Sanchez","species":"Human"}];
-
-
-})
+describe(filterSpecies, () => {
+      const sampleSpice = [{
+          "name": "Rick Sanchez",
+          "species": "Human"
+        },
+        {
+          "name": "Morty Smith",
+          "species": "Human"
+        }
+      ]
+      it('deberia retornar el objeto Rick Sanchez al filtrar por especie humano', () => {
+        assert.deepEqual(window.filterSpecies(sampleSpice, 'Human'), [{
+          "name": "Rick Sanchez",
+          "species": "Human"
+        }]);
+      });
+    });
